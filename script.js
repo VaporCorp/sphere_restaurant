afa9d3a88409c91e965954f3edda74e994e40a9e
Vapor <louisadressedev@gmail.com>
2024-04-26 07:11:46 +0200

    Files

--- script.js
+++ script.js
@@ -19,3 +19,45 @@ menuBtn.addEventListener('click', () => {
     menuBtn.classList.toggle('open');
     menu.classList.toggle('open');
 });
+
+const delay = 250
+
+const cursor = document.getElementById("custom-cursor")
+
+const links = document.querySelectorAll("a");
+
+links.forEach(link => {
+    link.addEventListener("mouseenter", function() {
+        cursor.classList.add("hovered");
+    });
+
+    link.addEventListener("mouseleave", function() {
+        cursor.classList.remove("hovered");
+    });
+});
+
+function getDimensions(e) {
+    cursor.style.top = `${e.clientY}px` // -25px for the size of the circle
+    cursor.style.left = `${e.clientX}px`
+}
+
+window.addEventListener("mousemove", (e) => {
+    getDimensions(e)
+});
+
+function throttle(callback, limit) {
+    let wait = false
+    return function () {
+        if (!wait) {
+            callback.apply(null, arguments)
+            wait = true
+            setTimeout(function () {
+                wait = false
+            }, limit)
+        }
+    }
+}
+
+window.addEventListener("mousemove", (e) => {
+    throttle(getDimensions(e), delay)
+});
