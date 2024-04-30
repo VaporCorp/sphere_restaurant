cc1e87101d22ac3825ad1ab5507a0af2a2c83918
Vapor <louisadressedev@gmail.com>
2024-04-30 09:45:46 +0200

    Files

--- assets/scripts/resize.js
+++ assets/scripts/resize.js
@@ -1,4 +1,4 @@
-window.addEventListener('resize', function() {
-    let button = document.querySelector('.header__button--booking');
-    button.style.top = `${window.innerHeight}px`;
+window.addEventListener('resize', function() {
+    let button = document.querySelector('.header__button--booking');
+    button.style.top = `${window.innerHeight}px`;
 });
